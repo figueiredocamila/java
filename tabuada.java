cc7f1674cb841bf0c611aa357ecac99189607430
Camila Dias Figueiredo <camila@MacBook-Pro-de-maxmilhas-2.local>
2021-08-12 20:45:48 -0300

    create simple codes

--- tabuada.java
+++ tabuada.java
@@ -1,1 +1,10 @@
+public class Tabuada {
+  public static void main(String[] args) {
 
+    for(int i = 0; i < 10; i++) {
+      String result = String.format("%s x 10 = %s.", i+1, (i+1)*10); 
+      System.out.println(result);
+    }
+    
+  }
+}
